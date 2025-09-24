302b998f16290653909cb847cc97c5abd59121c9
danieLayosh <danielayosh@gmail.com>
2025-09-25 01:32:19 +0300

    created convert_heb_year_to_gregorian for age calc

--- src/services/dateConvertionLogic.py
+++ src/services/dateConvertionLogic.py
@@ -17,3 +17,19 @@ def convert_hebrew_to_gregorian(hebrew_date: tuple) -> tuple:
     gregorian_date_obj = hebrew_date_obj.to_greg()
     return (gregorian_date_obj.year, gregorian_date_obj.month, gregorian_date_obj.day)
     
+
+def convert_heb_year_to_gregorian(heb_year: int, heb_month: int, heb_day: int) -> int:
+    """
+    Convert a Hebrew year to the corresponding Gregorian year.
+    The function assumes the Hebrew year starts in the fall of the Gregorian year.
+
+    Args:
+        heb_year (int): The Hebrew year to convert.
+    Returns:
+        int: The corresponding Gregorian year.  
+    """
+    # The Hebrew year starts in the fall of the Gregorian year
+    # So, we convert the 1st of Tishrei (7th month) of the Hebrew year to Gregorian
+    hebrew_date_obj = dates.HebrewDate(year=heb_year, month=heb_month, day=heb_day)  # 1st of Tishrei
+    gregorian_date_obj = hebrew_date_obj.to_greg()
+    return gregorian_date_obj.year
